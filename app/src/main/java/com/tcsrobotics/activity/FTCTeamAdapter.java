fba68172e5098d17376fe35106e349612841f9bb
Jay Steven  Hamilton <jhamilton@catalogicsoftware.com>
2016-01-24 12:21:11 -0500

    Implemented a filtered search. More work is needed to get the correct row data.

--- app/src/main/java/com/tcsrobotics/activity/FTCTeamAdapter.java
+++ app/src/main/java/com/tcsrobotics/activity/FTCTeamAdapter.java
@@ -4,44 +4,114 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 import com.tcsrobotics.domain.FTCTeam;
 import com.tcsrobotics.myapplication.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by jayhamilton on 1/4/16.
  */
-public class FTCTeamAdapter extends ArrayAdapter<FTCTeam> {
+public class FTCTeamAdapter extends BaseAdapter implements Filterable{
 
     private List<FTCTeam> teams;
+    private List<FTCTeam> orig;
+    public Context context;
 
-    public FTCTeamAdapter(Context context, int resource, List<FTCTeam> objects) {
 
-        super(context, resource, objects);
+    public FTCTeamAdapter(Context context, List<FTCTeam> teams) {
 
-        teams = objects;
+        super();
+        this.context = context;
+        this.teams = teams;
 
+    }
+
+    public class TeamHolder{
+
+        TextView vteamName;
+        TextView vteamId;
+        TextView vteamRank;
+    }
+
+    @Override
+    public int getCount() {
+        return teams.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return teams.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+
+        TeamHolder teamHolder;
+
         if(convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent,false);
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item, parent,false);
+            teamHolder = new TeamHolder();
+            teamHolder.vteamName = (TextView) convertView.findViewById(R.id.textTeamName);
+            teamHolder.vteamId = (TextView) convertView.findViewById(R.id.textTeamId);
+            teamHolder.vteamRank = (TextView) convertView.findViewById(R.id.textTeamRank);
+            convertView.setTag(teamHolder);
+
+        }else{
+
+            teamHolder = (TeamHolder) convertView.getTag();
         }
 
-        FTCTeam team = teams.get(position);
-        TextView teamName = (TextView) convertView.findViewById(R.id.textTeamName);
-        TextView teamNumber = (TextView) convertView.findViewById(R.id.textTeamId);
-        TextView teamRank = (TextView) convertView.findViewById(R.id.textTeamRank);
-        teamName.setText(team.getTeamName());
-        teamNumber.setText(team.getTeamId());
-        teamRank.setText(team.getTeamRank() + "");
+        teamHolder.vteamName.setText( teams.get(position).getTeamName());
+        teamHolder.vteamId.setText( teams.get(position).getTeamId());
+        teamHolder.vteamRank.setText( teams.get(position).getTeamRank() + "");
 
         return  convertView;
 
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                final FilterResults oReturn = new FilterResults();
+                final List<FTCTeam> results = new ArrayList<>();
+                if (orig == null)
+                    orig = teams;
+                if (constraint != null) {
+                    if (orig != null && orig.size() > 0) {
+                        for (final FTCTeam t : orig) {
+                            if (t.getTeamName().toLowerCase()
+                                    .contains(constraint.toString()) || t.getTeamId().toLowerCase().contains(constraint.toString()) )
+                                results.add(t);
+                        }
+                    }
+                    oReturn.values = results;
+                }
+                return oReturn;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                teams = (ArrayList<FTCTeam>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/tcsrobotics/activity/MainActivity.java
+++ app/src/main/java/com/tcsrobotics/activity/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 import com.tcsrobotics.domain.DataProvider;
 import com.tcsrobotics.domain.FTCTeam;
@@ -17,13 +19,15 @@ import com.tcsrobotics.myapplication.R;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     public static final String TEAM_ID = "TEAM_ID";
     public static final String OP_EDIT = "EDIT";
     public static final String OP_NEW = "NEW";
 
     List<FTCTeam> teams;
+    ListView listView;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        listView = (ListView) findViewById(R.id.listView);
+        searchView = (SearchView) findViewById(R.id.searchView);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -45,19 +52,34 @@ public class MainActivity extends AppCompatActivity {
 
         setTeams(DataProvider.teamList);
 
-        FTCTeamAdapter adapter = new FTCTeamAdapter(this, R.layout.list_item, getTeams());
-        ListView view = (ListView) findViewById(R.id.listView);
-        view.setAdapter(adapter);
-        view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        FTCTeamAdapter adapter = new FTCTeamAdapter(this, getTeams());
+        listView.setAdapter(adapter);
 
+        listView.setTextFilterEnabled(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                /* TODO
+                    This needs to be fixed. Now that I am using a filter I need to get the data id and not rely on
+                    position to get the correct row selected value
+                 */
                 callDetail(OP_EDIT, position);
-
             }
         });
+
+        setupSearchView();
+
     }
+
+    private void setupSearchView() {
+
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(this);
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setQueryHint("Search...");
+    }
+
 
     private void callDetail(String operation, int position) {
 
@@ -118,5 +140,20 @@ public class MainActivity extends AppCompatActivity {
 
     private void setTeams(List<FTCTeam> teams) {
         this.teams = teams;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            listView.clearTextFilter();
+        } else {
+            listView.setFilterText(newText);
+        }
+        return true;
     }
 }
